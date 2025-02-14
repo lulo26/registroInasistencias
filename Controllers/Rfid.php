916e50a7a67f64b5cd88abc3f43589474a025708
Juan David Luna Ramirez <jdluna9412@gmail.com>
2025-02-13 22:04:26 -0500

    insertar codigo

--- Controllers/Rfid.php
+++ Controllers/Rfid.php
@@ -13,8 +13,13 @@ class Rfid extends Controllers{
     public function buscarCodigo($codigo){
        $srtCodigo = strClean($codigo);
        $respuesta = $this->model->buscarCodigo($srtCodigo);
-       dep($respuesta);
        
+       if (!empty($respuesta)) {
+        $arrResponse = array('estado'=>true,'data'=>$srtCodigo);
+       } else {
+        $arrResponse = array('estado'=>false,'mensaje'=>"no se encontro el codigo: ".$srtCodigo);
+       }
+       echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
     }
 }
 ?>
--- Models/RfidModel.php
+++ Models/RfidModel.php
@@ -4,18 +4,15 @@ class RfidModel extends Mysql {
         parent::__construct();
 
     }
-/*     public function insertarCodigo($codigo,$fechaHora) {
-        $sql = "INSERT INTO rgistros_rfid (codigoRfid, fechaRfid) VALUES (:codigo, :fechaHora)";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bindParam(':codigo', $codigo);
-        $stmt->bindParam(':fechaHora', $fechaHora);
-        return $stmt->execute();
-    } */
+    public function insertarInasistencia($codigo,$fechaHora) {
+        
+    } 
     public function buscarCodigo(string $codigo){
         $this->codigo=$codigo;
-        $sql = "SELECT codigoRfid FROM rgistros_rfid WHERE id = '{$this->codigo}'";
+        $sql = "SELECT * FROM aprendices WHERE codigo_aprendiz = '{$this->codigo}'";
         $request = $this->select($sql);
         return $request;
     }
+
 }
 ?>
