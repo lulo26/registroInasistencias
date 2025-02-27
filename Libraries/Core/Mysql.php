55479605b11be792f85340cec765fce5aa791e53
ADSO <juandaniels305@gmail.com>
2025-02-27 07:36:32 -0500

    merge organizado

--- Libraries/Core/Mysql.php
+++ Libraries/Core/Mysql.php
@@ -1,16 +1,11 @@
 <?php
 
-<<<<<<< HEAD
 class Mysql extends Conexion
 {
-=======
-class Mysql extends Conexion{
->>>>>>> 670947a49ff133b601f3e0132ad63ddec737f499
     private $conexion;
     private $strquery;
     private $arrValues;
 
-<<<<<<< HEAD
     function __construct()
     {
         $this->conexion = new Conexion();
@@ -19,39 +14,21 @@ class Mysql extends Conexion{
 
     public function insert(string $query, array $arrValues)
     {
-=======
-    function __construct(){
-        $this->conexion = new Conexion();
-        $this->conexion = $this->conexion->conect();        
-    }
-
-    public function insert(string $query, array $arrValues){
->>>>>>> 670947a49ff133b601f3e0132ad63ddec737f499
         $this->strquery = $query;
         $this->arrValues = $arrValues;
 
         $insert = $this->conexion->prepare($this->strquery);
         $resInsert = $insert->execute($this->arrValues);
-<<<<<<< HEAD
         if ($resInsert) {
             $lastInsert = $this->conexion->lastInsertId();
         } else {
-=======
-        if($resInsert){
-            $lastInsert = $this->conexion->lastInsertId();
-        }else{
->>>>>>> 670947a49ff133b601f3e0132ad63ddec737f499
             $lastInsert = 0;
         }
 
         return $lastInsert;
     }
-<<<<<<< HEAD
     public function select(string $query)
     {
-=======
-    public function select(string $query){
->>>>>>> 670947a49ff133b601f3e0132ad63ddec737f499
         $this->strquery = $query;
         $result = $this->conexion->prepare($this->strquery);
         $result->execute();
@@ -59,12 +36,8 @@ class Mysql extends Conexion{
         return $data;
     }
 
-<<<<<<< HEAD
     public function select_all(string $query)
     {
-=======
-    public function select_all(string $query){
->>>>>>> 670947a49ff133b601f3e0132ad63ddec737f499
         $this->strquery = $query;
         $result = $this->conexion->prepare($this->strquery);
         $result->execute();
@@ -72,19 +45,14 @@ class Mysql extends Conexion{
         return $data;
     }
 
-<<<<<<< HEAD
     public function update(string $query, array $arrValues)
     {
-=======
-    public function update(string $query, array $arrValues){
->>>>>>> 670947a49ff133b601f3e0132ad63ddec737f499
         $this->strquery = $query;
         $this->arrValues = $arrValues;
         $update = $this->conexion->prepare($this->strquery);
         $resExecute = $update->execute($this->arrValues);
         return $resExecute;
     }
-<<<<<<< HEAD
     public function efectuarConsulta($consulta, $parametros = [], $tipos = '')
     {
         try {
@@ -114,17 +82,9 @@ class Mysql extends Conexion{
 
     public function delete(string $query)
     {
-=======
-
-    public function delete(string $query){
->>>>>>> 670947a49ff133b601f3e0132ad63ddec737f499
         $this->strquery = $query;
         $result = $this->conexion->prepare($this->strquery);
         $del = $result->execute();
         return $del;
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 670947a49ff133b601f3e0132ad63ddec737f499
--- Views/Template/footer_admin.php
+++ Views/Template/footer_admin.php
@@ -1,15 +1,15 @@
 <footer id="footer" class="footer">
-<<<<<<< HEAD
-  <div class="copyright">
+  <<<<<<< HEAD
+    <div class="copyright">
     &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-  </div>
-  <div class="credits">
-    <!-- All the links in the footer should remain intact. -->
-    <!-- You can delete the links only if you purchased the pro version. -->
-    <!-- Licensing information: https://bootstrapmade.com/license/ -->
-    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-  </div>
+    </div>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+    </div>
 </footer><!-- End Footer -->
 
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
@@ -41,34 +41,33 @@
 
 <!-- Script seteado de cada Controller -->
 <script src="<?= media(); ?>/js/<?= $data['page_functions_js'] ?>"></script>
-=======
-    <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-    </div>
-    <div class="credits">
-      <!-- All the links in the footer should remain intact. -->
-      <!-- You can delete the links only if you purchased the pro version. -->
-      <!-- Licensing information: https://bootstrapmade.com/license/ -->
-      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-    </div>
-  </footer><!-- End Footer -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+<div class="copyright">
+  &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+</div>
+<div class="credits">
+  <!-- All the links in the footer should remain intact. -->
+  <!-- You can delete the links only if you purchased the pro version. -->
+  <!-- Licensing information: https://bootstrapmade.com/license/ -->
+  <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+  Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+</div>
+</footer><!-- End Footer -->
 
-  <!-- Vendor JS Files -->
-  <script src="<?= media() ?>/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="<?= media() ?>/vendor/js/bootstrap.bundle.min.js"></script>
-  <script src="<?= media() ?>/vendor/chart.js/chart.umd.js"></script>
-  <script src="<?= media() ?>/vendor/echarts/echarts.min.js"></script>
-  <script src="<?= media() ?>/vendor/quill/quill.min.js"></script>
-  <script src="<?= media() ?>/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="<?= media() ?>/vendor/tinymce/tinymce.min.js"></script>
-  <script src="<?= media() ?>/vendor/php-email-form/validate.js"></script>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Template Main JS File -->
-  <script src="<?= media() ?>/js/main.js"></script>
->>>>>>> 670947a49ff133b601f3e0132ad63ddec737f499
+<!-- Vendor JS Files -->
+<script src="<?= media() ?>/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="<?= media() ?>/vendor/js/bootstrap.bundle.min.js"></script>
+<script src="<?= media() ?>/vendor/chart.js/chart.umd.js"></script>
+<script src="<?= media() ?>/vendor/echarts/echarts.min.js"></script>
+<script src="<?= media() ?>/vendor/quill/quill.min.js"></script>
+<script src="<?= media() ?>/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="<?= media() ?>/vendor/tinymce/tinymce.min.js"></script>
+<script src="<?= media() ?>/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="<?= media() ?>/js/main.js"></script>
 
 </body>
 
