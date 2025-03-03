b76e6b883118eb8d98ccfc9056ab94b571429631
Daniel Salazar <juan.salazar111111@soy.sena.edu.co>
2025-03-03 18:43:53 -0500

    Diseño de tabla aprendiz y mejora en los botones

--- Views/Template/modals/aprendizModal.php
+++ Views/Template/modals/aprendizModal.php
@@ -1,6 +1,7 @@
 <!-- Modal Crear/Actualizar -->
 <div class="modal fade" id="crearAprendizModal" name="crearAprendizModal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-scrollable">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+        <!-- Modal content -->
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" name="crearAprendizModalLabel" id="crearAprendizModalLabel">Crear Usuario</h5>
@@ -9,55 +10,70 @@
                 </button>
             </div>
             <div class="modal-body">
-                <!-- aquí va el contenido del modal -->
                 <form id="frmAprendiz" name="frmAprendiz">
                     <input type="hidden" id="idAprendiz" name="idAprendiz" value="0">
-                    <span>Todos los campos son obligatorios</span>
-                    <br><br>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="txtIdentificacion">Identificacion</label>
-                            <input type="number" class="form-control valid validNumber" id="numeroDocumentoAprendiz"
-                                name="numeroDocumentoAprendiz" autocomplete="off" required="">
+                    <div class="alert alert-info">
+                        <strong>Importante:</strong> Todos los campos son obligatorios.
+                    </div>
+                    <div class="form-row mb-3 justify-content-center">
+                        <div class="form-group col-md-6 text-center">
+                            <label for="numeroDocumentoAprendiz">Identificación <span
+                                    class="text-danger">*</span></label>
+                            <input type="number" class="form-control" id="numeroDocumentoAprendiz"
+                                name="numeroDocumentoAprendiz" autocomplete="off" required>
                         </div>
                     </div>
 
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="txtNombres">Nombres del aprendiz</label>
-                            <input type="text" class="form-control valid validText" id="nombreAprendiz"
-                                name="nombreAprendiz" autocomplete="off" required="">
-
+                    <div class="form-row mb-3 justify-content-center">
+                        <div class="form-group col-md-6 text-center">
+                            <label for="nombreAprendiz">Nombres del aprendiz <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="nombreAprendiz" name="nombreAprendiz"
+                                autocomplete="off" required>
                         </div>
                     </div>
-                    <div class="form-group col-md-6">
-                        <label for="txtApellidos">Apellidos del aprendiz</label>
-                        <input type="text" class="form-control valid validText" id="apellidoAprendiz"
-                            name="apellidoAprendiz" autocomplete="off" required="">
-                    </div>
-                    <div class="form-group col-md-6" name="usuario" id="contra">
-                        <label for="txtApellidos">Nombre de usuario</label>
-                        <input type="text" class="form-control valid validText" id="usuarioAprendiz"
-                            name="usuarioAprendiz" autocomplete="off" required="">
-                    </div>
-                    <div class="form-group col-md-6" name="contra" id="contra">
-                        <label for="txtApellidos">Contraseña</label>
-                        <input type="text" class="form-control valid validText" id="contraAprendiz"
-                            name="contraAprendiz" autocomplete="off" required="">
-                    </div>
-                    <div class="form-row">
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="txtApellidos">Codigo del aprendiz</label>
-                                <input type="text" class="form-control valid validText" id="codigoAprendiz"
-                                    name="codigoAprendiz" autocomplete="off" required="">
-                            </div>
-                        </div>
 
-                        <div class="form-group col-md-6">
-                            <label for="txtApellidos">Genero</label>
-                            <input type="text" class="form-control" id="generoAprendiz" name="generoAprendiz"
-                                autocomplete="off" required="">
+                    <div class="form-row mb-3 justify-content-center">
+                        <div class="form-group col-md-6 text-center">
+                            <label for="apellidoAprendiz">Apellidos del aprendiz <span
+                                    class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="apellidoAprendiz" name="apellidoAprendiz"
+                                autocomplete="off" required>
+                        </div>
+                    </div>
+
+                    <div class="form-row mb-3 justify-content-center">
+                        <div class="form-group col-md-6 text-center">
+                            <label for="usuarioAprendiz">Nombre de usuario <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="usuarioAprendiz" name="usuarioAprendiz"
+                                autocomplete="off" required>
+                        </div>
+                    </div>
+
+                    <div class="form-row mb-3 justify-content-center">
+                        <div class="form-group col-md-6 text-center">
+                            <label for="contraAprendiz">Contraseña <span class="text-danger">*</span></label>
+                            <input type="password" class="form-control" id="contraAprendiz" name="contraAprendiz"
+                                autocomplete="off" required>
+                        </div>
+                    </div>
+
+                    <div class="form-row mb-3 justify-content-center">
+                        <div class="form-group col-md-6 text-center">
+                            <label for="codigoAprendiz">Código del aprendiz <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="codigoAprendiz" name="codigoAprendiz"
+                                autocomplete="off" required>
+                        </div>
+                    </div>
+
+                    <div class="form-row mb-3 justify-content-center">
+                        <div class="form-group col-md-6 text-center">
+                            <label for="generoAprendiz">Género <span class="text-danger">*</span></label>
+                            <select class="form-control" id="generoAprendiz" name="generoAprendiz" required>
+                                <option value="">Seleccione un género</option>
+                                <option value="Masculino">Masculino</option>
+                                <option value="Femenino">Femenino</option>
+                                <option value="Otros">Otros..</option>
+                            </select>
                         </div>
                     </div>
 
@@ -65,11 +81,15 @@
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
                 <button type="submit" class="btn btn-primary">Guardar</button>
-                </form>
             </div>
+            </form>
         </div>
     </div>
 </div>
+
+
+
+
 
 <!-- Modal VER USUARIOS --><!-- 
 <div class="modal fade" id="verUsuarioModal" tabindex="-1" aria-hidden="true">
